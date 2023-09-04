4a886f04e8c82d862b80d302484ad8af339a5287
Sérgio Cardoso <sergioifbaiano@hotmail.com>
2023-09-03 23:48:16 -0300

    Código basic JWT

---
+++ src/main/java/com/springsecurity/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.springsecurity.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springsecurity.model.User;
+import com.springsecurity.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService service;
+    @PostMapping
+    public void postUser(@RequestBody User user){
+        service.createUser(user);
+    }
+}
---
+++ src/main/java/com/springsecurity/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.springsecurity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.springsecurity.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.username= (:username)")
+    public User findByUsername(@Param("username") String username);
+
+    boolean existsByUsername(String username);
+}
